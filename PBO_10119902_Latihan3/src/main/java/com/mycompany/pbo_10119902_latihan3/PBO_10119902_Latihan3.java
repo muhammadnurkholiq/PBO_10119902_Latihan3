c133963a3d5d1df28e39970cc52e7f9de72fbf15
muhammadnurkholiq <73203850+muhammadnurkholiq@users.noreply.github.com>
2020-10-21 10:32:52 +0700

    Add files via upload
    First Commit

---
+++ PBO_10119902_Latihan3/src/main/java/com/mycompany/pbo_10119902_latihan3/PBO_10119902_Latihan3.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo_10119902_latihan3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Muhammad Nurkholiq
+ */
+public class PBO_10119902_Latihan3 {
+    public static void main(String[] args) {
+        
+        System.out.print("Masukkan Nama Anda: ");
+        Scanner scanner = new Scanner(System.in);
+        
+        String nama = scanner.next();
+        System.out.println("Nama Anda Adalah "+nama);
+    }
+}
